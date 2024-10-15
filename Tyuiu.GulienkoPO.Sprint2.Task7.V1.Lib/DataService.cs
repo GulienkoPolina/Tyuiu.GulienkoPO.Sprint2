9aff2ef2aa24ee38b6cc74fe52518bd4024be7ad
Polina <gulienko.polina@mail.ru>
2024-10-15 22:02:34 +0500

    Task7

---
+++ Tyuiu.GulienkoPO.Sprint2.Task7.V1.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+using Tyuiu.GulienkoPO.Sprint2.Task7.V1.Lib;
+
+namespace Tyuiu.GulienkoPO.Sprint2.Task7.V1.Lib
+{
+    public class DataService : ISprint2Task7V1
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            return ((x * x) + (y * y) <= 1) && (y >= 0) && (y >= Math.Abs(x));
+        }
+    }
+}
